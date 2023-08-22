536eefe1cef02e95cef8bf1f8fc1b070e4a5a992
KeeStep <13651486829@163.com>
2023-08-22 23:00:41 +0800

    05 - 丰富BeanFactory

--- spring-keestep-03/src/test/java/org/kee/spring/test/Test05AutoWireBean.java
+++ spring-keestep-03/src/test/java/org/kee/spring/test/Test05AutoWireBean.java
@@ -1,6 +1,8 @@
 package org.kee.spring.test;
 
 import org.junit.Test;
+import org.kee.spring.beans.factory.surpport.DefaultListableBeanFactory;
+import org.kee.spring.beans.factory.xml.XmlBeanDefinitionReader;
 
 /**
  * Unit test for simple App.
@@ -11,6 +13,14 @@ public class Test05AutoWireBean {
      */
     @Test
     public void Test05AutoWireBean() {
-        // TODO
+        // TODO 创建文件 spring.xml
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2. 读取配置文件&注册Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
+
+        // 3. TODO 获取Bean对象调用方法
     }
 }
