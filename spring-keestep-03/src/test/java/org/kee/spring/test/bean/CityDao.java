05aacf737226fc2a0b5595a0223aef7c8aebbeeb
KeeStep <13651486829@163.com>
2023-08-31 23:26:05 +0800

    07 - Unit test

---
+++ spring-keestep-03/src/test/java/org/kee/spring/test/bean/CityDao.java
@@ -0,0 +1,31 @@
+package org.kee.spring.test.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *
+ * @author Eric
+ * @date 2023/8/13 23:20
+ */
+public class CityDao {
+
+    private static Map<String,String> hashMap = new HashMap<>();
+
+    public void initDataMethod() {
+        System.out.println("执行 initDataMethod");
+        hashMap.put("XIY", "西安市");
+        hashMap.put("SZX", "深圳市");
+        hashMap.put("PKG", "北京市");
+    }
+
+    public void destroyDataMethod() {
+        System.out.println("执行 destroyDataMethod");
+        hashMap.clear();
+    }
+
+    public String getCityName(String code) {
+        return hashMap.get(code);
+    }
+}
