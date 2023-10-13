a4b575a83721d7499c39bb73d81f558b466e5bf2
livingston <13651486829@163.com>
2023-10-13 22:29:55 +0800

    17 - 类型转换：实现 GenericConverter及GenericConversionService

--- src/main/java/org/kee/spring/core/convert/converter/GenericConverter.java
+++ src/main/java/org/kee/spring/core/convert/converter/GenericConverter.java
@@ -1,11 +1,64 @@
 package org.kee.spring.core.convert.converter;
 
+import cn.hutool.core.lang.Assert;
+
+import java.util.Objects;
+import java.util.Set;
+
 /**
- * <p>TODO 通用类型转换器
+ * <p> 通用类型转换器
  *
  * @author Eric
  * @date 2023/10/9 11:41 下午
  */
 public interface GenericConverter {
-    //  添加具体通用方法
+
+    /**
+     * 获取转换类型对的集合
+     * @return
+     */
+    Set<ConvertiblePair> getConvertibleTypes();
+
+    /**
+     * 类型转换
+     */
+    Object convert(Object source, Class sourceType, Class targetType);
+
+    /**
+     * 转换类型对
+     */
+    final class ConvertiblePair {
+        private final Class<?> sourceType;
+        private final Class<?> targetType;
+
+
+        public ConvertiblePair(Class<?> sourceType, Class<?> targetType) {
+            Assert.notNull(sourceType, "Source type must not be null");
+            Assert.notNull(targetType, "Target type must not be null");
+
+            this.sourceType = sourceType;
+            this.targetType = targetType;
+        }
+
+        public Class<?> getSourceType() {
+            return sourceType;
+        }
+
+        public Class<?> getTargetType() {
+            return targetType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ConvertiblePair that = (ConvertiblePair) o;
+            return sourceType.equals(that.sourceType) && targetType.equals(that.targetType);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(sourceType, targetType);
+        }
+    }
 }
--- src/main/java/org/kee/spring/core/convert/support/GenericConversionService.java
+++ src/main/java/org/kee/spring/core/convert/support/GenericConversionService.java
@@ -6,8 +6,12 @@ import org.kee.spring.core.convert.converter.ConverterFactory;
 import org.kee.spring.core.convert.converter.ConverterRegistry;
 import org.kee.spring.core.convert.converter.GenericConverter;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
+
 /**
- * <p> TODO 通用类型转换服务
+ * <p> 通用类型转换服务
  * Base ConversionService implementation suitable for use in most environments.
  * Indirectly implements ConverterRegistry as registration API through the
  * ConfigurableConversionService interface.
@@ -18,28 +22,147 @@ import org.kee.spring.core.convert.converter.GenericConverter;
  */
 public class GenericConversionService implements ConversionService, ConverterRegistry {
 
+    private Map<GenericConverter.ConvertiblePair, GenericConverter> converterMap = new HashMap<>();
+
     @Override
     public boolean canConvert(Class<?> sourceType, Class<?> targetType) {
-        return false;
+        return Objects.nonNull(getConverter(sourceType, targetType));
     }
 
     @Override
     public <T> T convert(Object source, Class<T> targetType) {
-        return null;
+        GenericConverter converter = getConverter(source.getClass(), targetType);
+        return (T) converter.convert(source, source.getClass(), targetType);
     }
 
     @Override
     public void addConverter(Converter<?, ?> converter) {
-
+        GenericConverter.ConvertiblePair typeInfo = getRequiredTypeInfo(converter);
+        ConverterAdapter converterAdapter = new ConverterAdapter(typeInfo, converter);
+        for (GenericConverter.ConvertiblePair convertibleType : converterAdapter.getConvertibleTypes()) {
+            converterMap.put(convertibleType, converterAdapter);
+        }
     }
 
     @Override
     public void addConverter(GenericConverter converter) {
-
+        for (GenericConverter.ConvertiblePair convertibleType : converter.getConvertibleTypes()) {
+            converterMap.put(convertibleType, converter);
+        }
     }
 
     @Override
     public void addConverterFactory(ConverterFactory<?, ?> converterFactory) {
+        GenericConverter.ConvertiblePair typeInfo = getRequiredTypeInfo(converterFactory);
+        ConverterFactoryAdapter converterFactoryAdapter = new ConverterFactoryAdapter(typeInfo, converterFactory);
+        for (GenericConverter.ConvertiblePair convertibleType : converterFactoryAdapter.getConvertibleTypes()) {
+            converterMap.put(convertibleType, converterFactoryAdapter);
+        }
+    }
 
+    /**
+     * 获取需要的类型对信息
+     * @param object
+     * @return
+     */
+    private GenericConverter.ConvertiblePair getRequiredTypeInfo(Object object) {
+        Type[] types = object.getClass().getGenericInterfaces();
+
+        ParameterizedType type = (ParameterizedType) types[0];
+        Type[] actualTypeArguments = type.getActualTypeArguments();
+
+        Class sourceType = (Class) actualTypeArguments[0];
+        Class targetType = (Class) actualTypeArguments[0];
+
+        return new GenericConverter.ConvertiblePair(sourceType, targetType);
+    }
+
+
+    /**
+     * 获取类型转换器
+     * @param sourceType
+     * @param targetType
+     * @return
+     */
+    protected GenericConverter getConverter(Class<?> sourceType, Class<?> targetType) {
+        List<Class<?>> sourceCandidates = getClassHierarchy(sourceType);
+        List<Class<?>> targetCandidates = getClassHierarchy(targetType);
+
+        // 遍历source与target类族，根据类型对获取类型转换器（k，v）
+        for (Class<?> sourceCandidate : sourceCandidates) {
+            for (Class<?> targetCandidate : targetCandidates) {
+                GenericConverter.ConvertiblePair convertiblePair = new GenericConverter.ConvertiblePair(sourceCandidate, targetCandidate);
+                GenericConverter converter = converterMap.get(convertiblePair);
+                if (null != converter) {
+                    return converter;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取类的 类族
+     * @param clazz
+     * @return
+     */
+    private List<Class<?>> getClassHierarchy(Class<?> clazz) {
+        List<Class<?>> hierarchy = new ArrayList<>();
+
+        while (null != clazz) {
+            hierarchy.add(clazz);
+            clazz = clazz.getSuperclass();
+        }
+
+        return hierarchy;
+    }
+
+    /**
+     * 转换器适配器
+     */
+    private final class ConverterAdapter implements GenericConverter {
+
+        private final ConvertiblePair typeInfo;
+        private final Converter<Object, Object> converter;
+
+        public ConverterAdapter(ConvertiblePair typeInfo, Converter<?, ?> converter) {
+            this.typeInfo = typeInfo;
+            this.converter = (Converter<Object, Object>) converter;
+        }
+
+        @Override
+        public Set<ConvertiblePair> getConvertibleTypes() {
+            return Collections.singleton(typeInfo);
+        }
+
+        @Override
+        public Object convert(Object source, Class sourceType, Class targetType) {
+            return converter.convert(source);
+        }
+    }
+
+    /**
+     * 转换工厂适配器
+     */
+    private final class ConverterFactoryAdapter implements GenericConverter {
+
+        private final GenericConverter.ConvertiblePair typeInfo;
+        private final ConverterFactory<Object, Object> converterFactory;
+
+        public ConverterFactoryAdapter(GenericConverter.ConvertiblePair typeInfo, ConverterFactory<?, ?> converterFactory) {
+            this.typeInfo = typeInfo;
+            this.converterFactory = (ConverterFactory<Object, Object>) converterFactory;
+        }
+
+
+        @Override
+        public Set<ConvertiblePair> getConvertibleTypes() {
+            return Collections.singleton(typeInfo);
+        }
+
+        @Override
+        public Object convert(Object source, Class sourceType, Class targetType) {
+            return converterFactory.getConverter(targetType).convert(source);
+        }
     }
 }
