1c88b42f44e34ffa21ac201aab4f9f4af6411646
KeeStep <13651486829@163.com>
2023-10-22 00:15:15 +0800

    18 - 单元测试：封装的JDBC操作QUERY

--- src/test/java/org/kee/spring/test/ApiTest.java
+++ src/test/java/org/kee/spring/test/ApiTest.java
@@ -3,15 +3,10 @@ package org.kee.spring.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.kee.spring.context.support.ClassPathXmlApplicationContext;
-import org.kee.spring.core.convert.converter.Converter;
-import org.kee.spring.core.convert.support.String2NumberConverterFactory;
 import org.kee.spring.jdbc.support.JdbcTemplate;
-import org.kee.spring.test.bean.Husband;
-import org.kee.spring.test.converter.String2IntegerConverter;
 
-import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Unit test for simple App.
@@ -28,13 +23,50 @@ public class ApiTest {
 
 
     @Test
-    public void ddlTest() throws SQLException {
+    public void ddlTest() {
         jdbcTemplate.execute(
                 "CREATE TABLE `city` (\n" +
                 "    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
                 "    `city_name` varchar(100) DEFAULT NULL,\n" +
                 "    PRIMARY KEY (`id`)\n" +
                 ") ENGINE=InnoDB COMMENT '城市信息表'");
+    }
+    
+    
+    @Test
+    public void queryForListTest() {
+        // FIXME
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from user");
+        for (int i = 0; i < maps.size(); i++) {
+            System.out.printf("第%d行数据" + maps.get(i), i + 1);
+        }
+    }
+    
+    @Test
+    public void queryForListWithColumnClassTypeTest() {
+        List<String> strings = jdbcTemplate.queryForList("select username from user", String.class);
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.printf("第%d行数据" + strings.get(i), i + 1);
+            System.out.println();
+        }
+    }
+    
+    @Test
+    public void queryForListWithColumnClassTypeAndArgTest() {
+        List<String> strings = jdbcTemplate.queryForList("select username from user where id =?", String.class, 2);
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.printf("第%d行数据" + strings.get(i), i + 1);
+            System.out.println();
+        }
+    }
+    
+    @Test
+    public void queryListWithArgTest() {
+        // FIXME
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from user where id =?", 2);
+        for (int i = 0; i < maps.size(); i++) {
+            System.out.printf("第%d行数据" + maps.get(i), i + 1);
+        }
     }
 }
 
