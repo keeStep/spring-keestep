f6110c17edfad4ef0542b32fb4b58476bea5ee2f
KeeStep <13651486829@163.com>
2023-10-23 23:59:04 +0800

    19 - 事务处理初始化

---
+++ src/main/java/org/kee/spring/tx/transaction/TransactionStatus.java
@@ -0,0 +1,29 @@
+package org.kee.spring.tx.transaction;
+
+import java.io.Flushable;
+import java.io.IOException;
+
+/**
+ * <p>
+ *
+ * @author Eric
+ * @date 2023/10/23 23:57
+ */
+public interface TransactionStatusextends extends SavepointManager, Flushable {
+
+    /**
+     * 是否开启新的事务
+     */
+    boolean isNewTransaction();
+
+    boolean hasSavepoint();
+
+    void setRollbackOnly();
+
+    boolean isRollbackOnly();
+
+    @Override
+    void flush() throws IOException;
+
+    boolean isCompleted();
+}
